882b35f009b6dd72030c80dbd4a14793524a1e52
pinguin-dev <mailfordevelopmentandstuff@gmail.com>
2024-02-14 00:52:25 +0500

    Update NanotrasenNameGenerator.cs

--- Content.Server/Maps/NameGenerators/NanotrasenNameGenerator.cs
+++ Content.Server/Maps/NameGenerators/NanotrasenNameGenerator.cs
@@ -11,7 +11,7 @@ public sealed partial class NanotrasenNameGenerator : StationNameGenerator
     /// </summary>
     [DataField("prefixCreator")] public string PrefixCreator = default!;
 
-    private string Prefix => "NT";
+    private string Prefix => "Orienta#";
     private string[] SuffixCodes => new []{ "LV", "NX", "EV", "QT", "PR" };
 
     public override string FormatName(string input)
@@ -19,6 +19,6 @@ public sealed partial class NanotrasenNameGenerator : StationNameGenerator
         var random = IoCManager.Resolve<IRobustRandom>();
 
         // No way in hell am I writing custom format code just to add nice names. You can live with {0}
-        return string.Format(input, $"{Prefix}{PrefixCreator}", $"{random.Pick(SuffixCodes)}-{random.Next(0, 999):D3}");
+        return string.Format(input, $"{Prefix}", $"{random.Next(0, 999):D3}");
     }
 }
