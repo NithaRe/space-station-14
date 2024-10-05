af10bf80409e81dba2fe5d0b1748422169100dce
NithaRe <olga.goryunova011@gmail.com>
2024-10-05 22:28:12 +0500

    revert better interaction + localefix

--- Content.Client/_Orienta/InteractionVerbs/InteractionVerbsSystem.cs
+++
@@ -1,8 +0,0 @@
-using Content.Shared._Orienta.InteractionVerbs;
-
-namespace Content.Client._Orienta.InteractionVerbs;
-
-// Just here because the shared system is abstract.
-public sealed class InteractionVerbsSystem : SharedInteractionVerbsSystem
-{
-}
